c0f1280f0a2529814ca7e8ce97e805f9cb7dac2f
marusr19 <112932595+marusr19@users.noreply.github.com>
2022-09-16 16:20:47 +0200

    tuto detalle

--- src/app/tutorial-detalle/tutorial-detalle.component.ts
+++ src/app/tutorial-detalle/tutorial-detalle.component.ts
@@ -24,4 +24,7 @@ export class TutorialDetalleComponent implements OnInit {
     this.tutorialService.putTutorial(this.tutorial);
   }
 
+  eliminarTutorial(){
+    this.tutorialService.deleteTutorial(this.tutorial.id);
+  }
 }
